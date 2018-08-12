9cdd85c54083a21df5bf29b98e66b29a0a924f0f
Arda Arman <ardaarman99@gmail.com>
2018-08-13 01:32:37 +0300

    initial commit

---
+++ javascript.js
@@ -0,0 +1,82 @@
+// DOM elements
+var selector = document.getElementById("brand-selector");
+var helperText = document.getElementById("helperText");
+
+var selectorButton = document.getElementById("brand-selector-button");
+
+var columns = document.querySelector(".columns");
+
+
+// Listens for brand selection
+selectorButton.addEventListener("click", function(e) {
+    // Clear screen
+    columns.innerHTML = "";
+    // Check for errors
+    if (!selector.value) {
+        helperText.textContent = "Please enter a brand name";
+        deleteFocus(selector);
+        return false;
+    }
+    var brand = selector.value.toLowerCase();
+    // Check if brand name exists in database
+
+    // Clear error message
+    helperText.textContent = "";
+    // Make request with selected brand name
+    var devicesPromise = makeRequest(parseUrl(brand));
+    devicesPromise.then(function(devices) {
+
+        // If response is empty array or if database tried to do autocomplete, display error message
+        if (devices.length === 1 || devices[0].DeviceName.split(" ")[0].toLowerCase() !== brand) {
+            helperText.textContent = "Brand name not found in database.";
+            deleteFocus(selector);
+            return false;
+        }
+        for (device of devices) {
+            var deviceColumn = document.createElement("div");
+            deviceColumn.classList.add("column", "has-text-centered");
+            var preparedDevice = prepareDeviceForDisplay(device);
+            for (property in preparedDevice) {
+                deviceColumn.textContent += preparedDevice[property] + "\n";
+            }
+            columns.appendChild(deviceColumn);
+        }
+        selector.value = "";
+        deleteFocus(selector);
+        e.preventDefault();
+    });
+
+
+    // Parses the given brand name and prepares ajax url
+    // Url return 5 latest devices from brand
+    function parseUrl(selectedBrand) {
+        return "https://fonoapi.freshpixl.com/v1/getlatest?brand=" + selectedBrand + "&limit=5&token=dee37f8fd64a0366bbbb958214e1a8aea0f3c4e53fc9d945"
+    }
+
+    // Function makes HTTP get request to an url
+    // Turns the value into JSON array
+    function makeRequest(url) {
+        return fetch(url).then(function(response) {
+            return response.json();
+        });
+    }
+
+    // Takes an Array from the API and prepares an object for displaySection
+    function prepareDeviceForDisplay(device) {
+        let resultObject = {
+            name: device.DeviceName
+        };
+        if (device.price) {
+            resultObject.price = device.price.replace("About ", "");
+        }
+        resultObject.size = device.size.split(" ")[0] + "\"";
+        resultObject.os = device.os.split(/[,;]/)[0];
+
+        return resultObject;
+    }
+});
+
+function deleteFocus(element) {
+    element.value = "";
+    element.focus();
+}
